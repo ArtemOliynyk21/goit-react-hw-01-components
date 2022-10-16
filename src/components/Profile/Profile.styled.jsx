fa2245628103363914f58d1f362a2add24561b74
ArtemOliynyk21 <temaneo700@gmail.com>
2022-10-16 23:27:00 +0300

    task-1-done

---
+++ src/components/Profile/Profile.styled.jsx
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Description = styled.div`
+  text-align: center;
+`;
+
+export const Avatar = styled.img`
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const Name = styled.p`
+    font-size: 24px;
+`
+
+export const Tag = styled.p`
+  padding: 10px;
+`;
+
+export const Location = styled.p`
+  color: gray;
+`;
+
+export const StatsList = styled.ul`
+  display: flex;
+  justify-content: center;
+`;
+
+export const StatsItem = styled.li`
+  display: flex;
+  align-items: center;
+`;
+
+export const Label = styled.span`
+ font-size: 18px;
+`;
+
+export const Quantity = styled.span`
+  font-size: 22px;
+`;
+
+
+
